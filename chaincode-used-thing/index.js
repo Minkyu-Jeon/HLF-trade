eeec01f3532cde5e960eb652aa7bae5dc83e25ac
Minkyu Jeon <minkyujeon9@gmail.com>
2021-03-17 18:02:20 +0900

    add used-thing chaincode

---
+++ chaincode-used-thing/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const UsedThingChainCode = require('./lib/UsedThingChainCode.js');
+
+module.exports.UsedThingChainCode = UsedThingChainCode;
+module.exports.contracts = [UsedThingChainCode];
