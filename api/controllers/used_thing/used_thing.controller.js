ba57c5f787f53b183e83bbe00fc059b84b799f62
Minkyu Jeon <minkyujeon9@gmail.com>
2021-03-18 16:33:40 +0900

    add used_thing read feature

--- api/controllers/used_thing/used_thing.controller.js
+++ api/controllers/used_thing/used_thing.controller.js
@@ -9,88 +9,6 @@ const chainCodeName = 'used_thing_cc'
 
 const UsedThingController = () => {
 
-  const destroy = async (req, res, next) => {
-    let result
-    try {
-      const ccp = buildCCPOrg(['../', 'config', 'connection-org1.json'])
-
-      const walletPath = path.join(process.cwd(), 'wallets')
-      
-      const wallet = await buildWallet(Wallets, walletPath)
-
-      const gateway = new Gateway()
-
-      try {
-        await gateway.connect(ccp, {
-          wallet: wallet,
-          identity: req.user.email,
-          discovery: { enabled: true, asLocalhost: false }
-        })
-
-        const network = await gateway.getNetwork(channelName)
-
-        const contract = network.getContract(chainCodeName)
-        
-        result = await contract.submitTransaction('DeleteAsset', req.params.id)
-        console.log('*** Result: committed')
-        if ( `${result}` !== '' ) {
-          console.log(`*** Result: ${result.toString()}`)
-        }
-        result = JSON.parse(result.toString())
-      } catch (err) {
-        throw new Error(err.message)
-      }
-       finally {
-        gateway.disconnect()
-      }
-    } catch (err) {
-      return helper.errorResponse(req, res, [err.message], 400, err)
-    }
-    return helper.successResponse(req, res, result, 0)
-  }
-
-  const update = async (req, res, next) => {
-    let result
-    try {
-      const ccp = buildCCPOrg(['../', 'config', 'connection-org1.json'])
-
-      const walletPath = path.join(process.cwd(), 'wallets')
-      
-      const wallet = await buildWallet(Wallets, walletPath)
-
-      const gateway = new Gateway()
-
-      try {
-        await gateway.connect(ccp, {
-          wallet: wallet,
-          identity: req.user.email,
-          discovery: { enabled: true, asLocalhost: false }
-        })
-
-        const network = await gateway.getNetwork(channelName)
-
-        const contract = network.getContract(chainCodeName)
-        
-        const createAssetParams = [req.params.id, req.body.color, req.body.size, req.body.owner, req.body.appraisedValue]
-
-        result = await contract.submitTransaction('UpdateAsset', ...createAssetParams)
-        console.log('*** Result: committed')
-        if ( `${result}` !== '' ) {
-          console.log(`*** Result: ${result.toString()}`)
-        }
-        result = JSON.parse(result.toString())
-      } catch (err) {
-        throw new Error(err.message)
-      }
-       finally {
-        gateway.disconnect()
-      }
-    } catch (err) {
-      return helper.errorResponse(req, res, [err.message], 400, err)
-    }
-    return helper.successResponse(req, res, result, 0)
-  }
-
   const show = async (req, res, next) => {
     let result
     try {
@@ -113,7 +31,7 @@ const UsedThingController = () => {
 
         const contract = network.getContract(chainCodeName)
 
-        result = await contract.evaluateTransaction('ReadAsset', req.params.id)
+        result = await contract.evaluateTransaction('Show', req.params.id)
         console.log('*** Result: committed')
         if ( `${result}` !== '' ) {
           console.log(`*** Result: ${result.toString()}`)
@@ -228,8 +146,6 @@ const UsedThingController = () => {
     register: register,
     index: index,
     show: show,
-    update: update,
-    destroy: destroy,
   }
 }
 
