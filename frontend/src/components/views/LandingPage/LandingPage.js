60186ee6d43b4ba4ebee490878ee800a3a043b77
Minkyu-Jeon <minkyujeon9@gmail.com>
2021-03-22 14:39:24 +0900

    frontend: 로그인 기능 추가

--- frontend/src/components/views/LandingPage/LandingPage.js
+++ frontend/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 function LandingPage() {
   return (
-    <div>
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
+    }}>
       LandingPage
     </div>
   )
